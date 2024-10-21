aeebf50e792d6f56fad333bad38aefb6cfff87ca
realslimtaek <89964264+realslimtaek@users.noreply.github.com>
2024-10-21 15:16:42 +0900

    Time: 0 ms (100.00%), Space: 12.5 MB (17.54%) - LeetHub

--- 0412-fizz-buzz/0412-fizz-buzz.py
+++ 0412-fizz-buzz/0412-fizz-buzz.py
@@ -1,9 +1,3 @@
 class Solution(object):
     def fizzBuzz(self, n):
-        li = []
-        for i in range(1,n+1) :
-            if i % 15 == 0 : li.append("FizzBuzz")
-            elif i % 3 ==0 : li.append("Fizz")
-            elif i % 5 == 0 : li.append("Buzz")
-            else : li.append(str(i))
-        return li
+        return ["FizzBuzz" if i % 15 == 0 else "Fizz" if i % 3 == 0 else "Buzz" if i%5 == 0 else str(i) for i in range(1,n+1)]
