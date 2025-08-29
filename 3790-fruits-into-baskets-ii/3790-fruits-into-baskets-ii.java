ff422c63a740c949b81c61bd0973633065e7ac17
realslimtaek <89964264+realslimtaek@users.noreply.github.com>
2025-08-29 14:56:00 +0900

    Time: 1 ms (100%), Space: 44.5 MB (47.87%) - LeetHub

--- 3790-fruits-into-baskets-ii/3790-fruits-into-baskets-ii.java
+++ 3790-fruits-into-baskets-ii/3790-fruits-into-baskets-ii.java
@@ -5,12 +5,11 @@ class Solution {
             boolean flag = false;
             for(int j = 0; j< baskets.length; j++) {
                 if(flag) break;
-                if(fruit > baskets[j]) continue;
-                else {
+                if(fruit <= baskets[j]) {
                     baskets[j] = 0;
                     flag = true;
                     break;
-                }
+                } else continue;
             }
 
             if(!flag) ans += 1;
