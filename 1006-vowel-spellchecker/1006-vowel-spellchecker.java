239f33c49bac9d0585d0999657ef8bd8246eb35a
realslimtaek <89964264+realslimtaek@users.noreply.github.com>
2025-09-14 17:28:56 +0900

    1006-VOWEL-SPELLCHECKER Stats: Time: 55 ms (21.8%), Space: 52.9 MB (14.22%) - LeetHub

--- 1006-vowel-spellchecker/1006-vowel-spellchecker.java
+++ 1006-vowel-spellchecker/1006-vowel-spellchecker.java
@@ -1,42 +1,44 @@
-import java.util.*;
-
 class Solution {
+
     public String[] spellchecker(String[] wordlist, String[] queries) {
-        Set<String> words = new HashSet<>(Arrays.asList(wordlist));
-        Map<String, String> caseInsensitive = new HashMap<>();
-        Map<String, String> vowelInsensitive = new HashMap<>();
-
-        for (String word : wordlist) {
-            String lower = word.toLowerCase();
-            caseInsensitive.putIfAbsent(lower, word);
-
-            String noVowel = lower.replaceAll("[aeiou]", "_");
-            vowelInsensitive.putIfAbsent(noVowel, word);
-        }
 
         String[] answer = new String[queries.length];
-        for (int i = 0; i < queries.length; i++) {
-            String query = queries[i];
 
-            if (words.contains(query)) {
+        Set<String> words = new HashSet<>(Arrays.asList(wordlist));
+        Map<String, String> lowerMap = new HashMap<>();
+        Map<String, String> noVowelMap = new HashMap<>();
+
+
+        for (String s : wordlist) {
+            String lower = s.toLowerCase();
+            lowerMap.putIfAbsent(lower, s);
+            String noVowel = lower.replaceAll("[aeiou]", "_");
+            noVowelMap.putIfAbsent(noVowel, s);
+        }
+
+
+        for(int i = 0; i < queries.length; i++) {
+            String query = queries[i];
+            if(words.contains(query)) {
                 answer[i] = query;
                 continue;
             }
 
             String lower = query.toLowerCase();
-            if (caseInsensitive.containsKey(lower)) {
-                answer[i] = caseInsensitive.get(lower);
+            if(lowerMap.containsKey(lower)) {
+                answer[i] = lowerMap.get(lower);
                 continue;
             }
 
-            String noVowel = lower.replaceAll("[aeiou]", "_");
-            if (vowelInsensitive.containsKey(noVowel)) {
-                answer[i] = vowelInsensitive.get(noVowel);
+            String noVowel = lower.replaceAll("[aeiou]","_");
+            if(noVowelMap.containsKey(noVowel)) {
+                answer[i] = noVowelMap.get(noVowel);
                 continue;
             }
 
             answer[i] = "";
         }
+
         return answer;
     }
 }
