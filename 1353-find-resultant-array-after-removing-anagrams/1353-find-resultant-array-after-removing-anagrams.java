574edd8a9828bfc9f166b45b88709c9041834879
realslimtaek <89964264+realslimtaek@users.noreply.github.com>
2025-10-13 17:01:10 +0900

    Time: 2 ms (93.48%), Space: 44 MB (99.78%) - LeetHub

--- 1353-find-resultant-array-after-removing-anagrams/1353-find-resultant-array-after-removing-anagrams.java
+++ 1353-find-resultant-array-after-removing-anagrams/1353-find-resultant-array-after-removing-anagrams.java
@@ -3,17 +3,14 @@ class Solution {
 
         List<String> answer = new ArrayList<>();
 
-        List<Map<Character, Integer>> anagramList = new ArrayList<>();
-
+        char[] chars, prev;
+        prev  = new char[1];
         for (int i = 0; i < words.length; i++) {
-            Map<Character, Integer> count = new HashMap<>();
 
-            for (char c : words[i].toCharArray()) {
-                count.put(c, count.getOrDefault(c, 0) + 1);
-            }
-            anagramList.add(count);
-
-            if (i == 0 || !anagramList.get(i - 1).equals(count)) {
+            chars = (words[i].toCharArray());
+            Arrays.sort(chars);
+            if (i == 0 || !Arrays.equals(prev, chars)) {
+                prev = chars;
                 answer.add(words[i]);
             }
         }
