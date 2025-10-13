d8007ef656fdfd08547f46653ce4fe61332b1e53
realslimtaek <89964264+realslimtaek@users.noreply.github.com>
2025-10-13 17:38:30 +0900

    Time: 0 ms (100%), Space: 40.7 MB (28.68%) - LeetHub

--- 1642-water-bottles/1642-water-bottles.java
+++ 1642-water-bottles/1642-water-bottles.java
@@ -1,7 +1,7 @@
 class Solution {
     public int numWaterBottles(int numBottles, int numExchange) {
         int answer = numBottles;
-        while(numBottles / numExchange >= 1) {
+        while(numBottles >= numExchange) {
             int temp = numBottles/numExchange;
             answer += temp;
             numBottles = numBottles/numExchange + numBottles%numExchange;
