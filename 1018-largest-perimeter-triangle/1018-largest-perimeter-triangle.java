1e09654cc4d5126c5d05a0c568aea7dcc19ef627
realslimtaek <89964264+realslimtaek@users.noreply.github.com>
2025-10-13 17:35:32 +0900

    Time: 38 ms (5.13%), Space: 45.8 MB (9.43%) - LeetHub

--- 1018-largest-perimeter-triangle/1018-largest-perimeter-triangle.java
+++ 1018-largest-perimeter-triangle/1018-largest-perimeter-triangle.java
@@ -7,13 +7,9 @@ class Solution {
                 .mapToInt(Integer::intValue)
                 .toArray();
 
-        for (int i = 0; i < nums.length; i++) {
-            for (int j = i + 1; j < nums.length; j++) {
-                for (int k = j + 1; k < nums.length; k++) {
-                    if (nums[i] < nums[j] + nums[k])
-                        return nums[i] + nums[j] + nums[k];
-                }
-            }
+        for (int i = 0; i < nums.length - 2; i++) {
+            if (nums[i] < nums[i + 1] + nums[i + 2])
+                return nums[i] + nums[i + 1] + nums[i + 2];
         }
         return 0;
     }
