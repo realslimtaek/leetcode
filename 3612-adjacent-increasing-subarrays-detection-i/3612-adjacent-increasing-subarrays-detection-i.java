be6ef2088ffd5d32bc7890a29bafd2713a928d64
realslimtaek <89964264+realslimtaek@users.noreply.github.com>
2025-10-14 11:24:50 +0900

    Time: 33 ms (5.15%), Space: 45.2 MB (7.35%) - LeetHub

--- 3612-adjacent-increasing-subarrays-detection-i/3612-adjacent-increasing-subarrays-detection-i.java
+++ 3612-adjacent-increasing-subarrays-detection-i/3612-adjacent-increasing-subarrays-detection-i.java
@@ -1,36 +1,15 @@
 class Solution {
     public boolean hasIncreasingSubarrays(List<Integer> nums, int k) {
-
-        int y = -1001;
-        int cnt = 0;
-        int tot = 0;
-        for (int i = 0; i < nums.size(); i++) {
-            if (y >= nums.get(i)) {
-                y = -1001;
-                cnt = 0;
-            }
-
-            y = nums.get(i);
-            cnt += 1;
-
-            if (cnt == k) {
-                if (i + k >= nums.size())
-                    break;
-                boolean t = checkB(nums, i + 1, k);
-                if (t)
-                    return true;
-                else {
-                    i = i - k + 1;
-                    cnt = 1;
-                }
-            }
+        for (int i = 0; i + 2 * k <= nums.size(); i++) {
+            if(check(nums, i , k) && check(nums, i + k, k)) return true;
         }
 
         return false;
     }
 
-    public boolean checkB(List<Integer> nums, int idx, int k) {
-        for (int i = idx+1; i < idx+ k ; i++) {
+    public boolean check(List<Integer> nums, int idx, int k) {
+        System.out.println(idx);
+        for (int i = idx+1; i < idx + k ; i++) {
             if(nums.get(i) <= nums.get(i-1)) return false;
         }
         return true;
