6d0a6eab577bd1ce79823247b3594cbe06de1230
realslimtaek <89964264+realslimtaek@users.noreply.github.com>
2025-10-15 15:53:46 +0900

    Time: 14 ms (95.74%), Space: 88.6 MB (54.26%) - LeetHub

--- 3619-adjacent-increasing-subarrays-detection-ii/3619-adjacent-increasing-subarrays-detection-ii.java
+++ 3619-adjacent-increasing-subarrays-detection-ii/3619-adjacent-increasing-subarrays-detection-ii.java
@@ -8,22 +8,19 @@ class Solution {
         for (int i = 0; i < size - 1; i++) {
 
             if (nums.get(i) >= nums.get(i + 1)) {
-                if (cnt > ans) {
-                    ans = calc(ans, cnt, own);
-                }
+                ans = calc(ans, cnt, own);
                 own = cnt;
                 cnt = 1;
                 continue;
-            }
-            cnt++;
+            } else cnt++;
         }
-        if (cnt > ans) {
-            ans = calc(ans, cnt, own);
-        }
+        
+        ans = calc(ans, cnt, own);
         return ans != 0 ? ans : size / 2;
     }
 
     public int calc(int a, int b, int c) {
+        if (a >= b) return a;
         int abMin = Math.min(a, b);
         int abHalfMax = Math.max(a / 2, b / 2);
         return Math.max(Math.min(c, b), Math.max(abMin, abHalfMax));
