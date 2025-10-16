21c121ff4a492f47324e33e37a6a7025a37fd2f8
realslimtaek <89964264+realslimtaek@users.noreply.github.com>
2025-10-16 16:09:38 +0900

    Time: 59 ms (38.58%), Space: 56.5 MB (100%) - LeetHub

--- 2661-smallest-missing-non-negative-integer-after-operations/2661-smallest-missing-non-negative-integer-after-operations.java
+++ 2661-smallest-missing-non-negative-integer-after-operations/2661-smallest-missing-non-negative-integer-after-operations.java
@@ -1,30 +1,29 @@
 class Solution {
     public int findSmallestInteger(int[] nums, int value) {
-        boolean[] check = new boolean[505050];
+        boolean[] check = new boolean[nums.length];
 
         Map<Integer, Integer> countMap = new HashMap<>();
 
         for (int now : nums) {
+            
             if (now < 0) {
                 int temp = Math.abs(now) % value;
                 now = temp == 0 ? 0 : value - temp;
             } else {
                 now = now % value;
             }
-            countMap.put(now, countMap.getOrDefault(now, 0) + 1);
-        }
-        System.out.println(countMap);
-        countMap.forEach( (k, v) -> {
-            for (int i = 0; i < v; i++) {
-                int tmp = value*i+k;
-                if(tmp >= check.length) break;
-                check[tmp] = true;
+
+            countMap.put(now, countMap.getOrDefault(now, -1) + 1);
+
+            if ((value * (countMap.get(now)) + now) < check.length) {
+                check[value * (countMap.get(now)) + now] = true;
             }
-        });
-        
+        }
+
         int ans = 0;
-        for(boolean b : check) {
-            if(!b) break;
+        for (boolean b : check) {
+            if (!b)
+                break;
             ans++;
         }
 
